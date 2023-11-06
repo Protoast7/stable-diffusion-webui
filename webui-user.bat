0a77ec87a0aee58dd7408af78f97f0b5196268c8
Protoast7 <agoogy99@gmail.com>
2023-11-06 17:45:43 -0600

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set PYTHON=
-set GIT=
-set VENV_DIR=
-set COMMANDLINE_ARGS=
+set PYTHON="C:\Program Files\Git\git-bash.exe"
+set GIT="C:\Program Files\Git\git-bash.exe"
+set VENV_DIR=I:\Programs\ai\stable-diffusion-webui\venv
+set COMMANDLINE_ARGS= --precision full --no-half
 
 call webui.bat
